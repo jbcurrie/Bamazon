81c5c9b0e01df755c614b9efb115c34b8a460ea7
jbcurrie <biobacker30@gmail.com>
2017-08-17 13:50:47 -0400

    minor changes to table styling

--- schema.sql
+++ schema.sql
@@ -7,30 +7,21 @@ CREATE TABLE products(
     product_name VARCHAR(45) NULL,
     style_color VARCHAR (45) NULL,
     department_name VARCHAR(45) NULL,
-    price DECIMAL(4,2),
+    price DECIMAL(5,2) NULL,
     stock_quantity INT NOT NULL,
+    product_sales DECIMAL(7,2) NULL,
     PRIMARY KEY (item_id)
 );
-
-ALTER TABLE products
-    MODIFY price DECIMAL(5,2) NULL;
-
-ALTER TABLE products
-    MODIFY product_sales DECIMAL(7,2);
 
 SELECT * FROM products;
 -- TRUNCATE TABLE products;
 
-
 CREATE TABLE departments(
     department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(45) NULL,
-    over_head_costs DECIMAL(5,2) NULL,
+    over_head_costs DECIMAL(7,2) NULL,
     PRIMARY KEY (department_id)
 );
-
-ALTER TABLE departments
-    MODIFY over_head_costs DECIMAL(7,2) NULL;
 
 SELECT * FROM departments;
 -- TRUNCATE TABLE departments;
